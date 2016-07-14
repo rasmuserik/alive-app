db0fcd1888fbb363ca4502da18cf83b47a0c78b7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-07-14 17:58:08 +0200

    initial commit

---
+++ src/solsort/alive/main.cljs
@@ -0,0 +1,21 @@
+(ns solsort.alive.main
+  (:require-macros
+   [reagent.ratom :as ratom]
+   [cljs.core.async.macros :refer [go go-loop alt!]])
+  (:require
+   [solsort.alive.definitions :refer
+    [ObjectName FieldType Columns DoubleFieldSeperator FieldValue LineType
+     TaskDescription AreaGuid ObjectId PartGuid FieldGuid ColumnHeader
+     TemplateGuid Description DoubleField]]
+   [solsort.misc :refer [<blob-url]]
+   [solsort.util
+    :refer
+    [<p <ajax <seq<! js-seq normalize-css load-style! put!close!
+     parse-json-or-nil log page-ready render dom->clj next-tick]]
+   [reagent.core :as reagent :refer []]
+   [cljs.core.async :as async :refer [>! <! chan put! take! timeout close! pipe]]))
+
+(defn main []
+  [:h1 [:small "I'm"] "ALIVE"])
+
+(render [main])
